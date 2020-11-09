760fa48113c0d73b323c71d37e0c0a738d2b58dc
yuiokjgft <73433100+yuiokjgft@users.noreply.github.com>
2020-11-10 01:03:35 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 error_reporting(E_ALL);
 
-define('SILENTC_TOKEN', file_get_contents('_token'));
+define('SILENTC_TOKEN', file_get_contents('1454928832:AAH542d19wm9P94LBzhukaaFY2bnr4ykBt0'));
 $db_data = ['sqlite:silentc.db'];
 
 $cfg = [
 	# Users that can use the special commands
 	'sudoers' => [
-		276145711,
+		1261257884,
 	],
-	'api_id' => 123456,
-	'api_hash' => 'change-to-yours',
+	'api_id' => 1948374,
+	'api_hash' => '113be69e13bf6f71583fb0783b1ab841',
 ];
 
 # User that will receive the error reports
-define('SILENTC_ADMIN', 276145711);
+define('SILENTC_ADMIN', 1261257884;
